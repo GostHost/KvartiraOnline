4c124bb8afadc648303e24c2db96ca16b297b8b1
GostHost <calmghost@yandex.ru>
2023-02-15 18:37:59 +0400

    Initial commit

---
+++ KvartiraOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.KvartiraOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvartiraOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvartiraOnlineApplication.class, args);
+	}
+
+}
