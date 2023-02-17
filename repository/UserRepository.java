247411f176349dc288e680dc5488f49898792fcf
GostHost <calmghost@yandex.ru>
2023-02-17 16:54:35 +0400

    Add User Repository

---
+++ repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.KvartiraOnline.repository;
+
+import com.example.KvartiraOnline.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository <User, Long> {
+
+    User findByName(String username);
+
+    @Override
+    void delete (User user);
+}
