b2706fd0bb39731a4f2c493c8ad523ffd832c61f
GostHost <calmghost@yandex.ru>
2023-02-19 10:12:01 +0400

    Add Privilege entity

---
+++ entity/Privilege.java
@@ -0,0 +1,51 @@
+package com.example.KvartiraOnline.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+public class Privilege {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "privileges")
+    private Collection<Role> roles;
+
+    public Privilege() {
+        super();
+    }
+
+    public Privilege(final String name) {
+        super();
+        this.name = name;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+}
