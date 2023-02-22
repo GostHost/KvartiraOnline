a8e5bb8b8b81e3a552eec2288f1b54b806b33df4
GostHost <calmghost@yandex.ru>
2023-02-22 09:27:16 +0400

    Add HomeController

--- config/MvcConfig.java
+++ config/MvcConfig.java
@@ -10,7 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("");
+
         registry.addViewController("/login").setViewName("login");
 
 
